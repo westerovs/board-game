af4533a20e457426c502ddb4f4b204560f15f74a
Александр <westerovs@mail.ru>
2021-07-17 20:57:33 +0300

    save

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,54 @@
-const render = (container, template, place = 'beforeend') => {
-    if (container instanceof Element) {
-        container.insertAdjacentHTML(place, template)
-    }
-}
-console.log('test template')
-
+// let position = 0
+// const sliderToShow = 8
+// const sliderToScroll = 1
+// const container = document.querySelector('.slider-row')
+// const track = document.querySelector('.slider-list')
+//
+// const btnPrev = document.querySelector('.btn-prev')
+// const btnNext = document.querySelector('.btn-next')
+// const items = document.querySelectorAll('.slider-item')
+// const itemsCount = items.length
+// const itemWidth = container.clientWidth / sliderToShow
+// const movePosition = sliderToScroll * itemWidth
+//
+// items.forEach(item => item.style.minWidth = `${ itemWidth }px`)
+//
+// btnNext.addEventListener('click', () => {
+//     const itemsLeft = itemsCount - (Math.abs(position) + sliderToShow * itemWidth) / itemWidth
+//
+//     position -= itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth
+//
+//     setPosition()
+//     checkBtns()
+// })
+//
+// btnPrev.addEventListener('click', () => {
+//     const itemsLeft = Math.abs(position) / itemWidth
+//
+//     position += itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth
+//
+//     setPosition()
+//     checkBtns()
+// })
+//
+// const setPosition = () => {
+//     track.style.transform = `translateX(${ position }px)`
+// }
+//
+// const checkBtns = () => {
+//     btnPrev.disabled = position === 0
+//     btnNext.disabled = position <= -(itemsCount - sliderToShow) * itemWidth
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
